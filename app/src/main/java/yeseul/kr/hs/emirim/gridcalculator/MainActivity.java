b89ea0acc009d3d012657a8c1cba356f407ba7e9
leeysi <yeseul726@e-mirim.hs.kr>
2017-04-10 20:41:16 +0900

    #3 Event Programming 1

--- app/src/main/java/yeseul/kr/hs/emirim/gridcalculator/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/gridcalculator/MainActivity.java
@@ -1,46 +1,91 @@
 package yeseul.kr.hs.emirim.gridcalculator;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import static yeseul.kr.hs.emirim.gridcalculator.R.id.but0;
-
-public class MainActivity extends AppCompatActivity{
-    Button[] butNums=new Button[10];
-    Button[] butOps=new Button[4];
-    //int[] ids={R.id.but0, R.id.but1, R.id.but2, R.id.but3, R.id.but4, R.id.but5, R.id.but6, R.id.but7, R.id.but8, R.id.but9}
+public class MainActivity extends AppCompatActivity {
+    EditText edit_1, edit_2;
+    Button[] butNums = new Button[10];
+    Button[] butOps = new Button[4];
+    public static final int SELECT_EDIT1=0;
+    public static final int SELECT_EDIT2=1;
+    int SelectEdit = SELECT_EDIT1;
+    String numStr = "";
+    // int[] ids = {R.id.but_0..... but_9까지 값을 초기화 해준다. => 미개한 방법} => for문 ids[i]
+    Button but_plus, but_minus, but_multiply, but_division;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit_1 = (EditText) findViewById(R.id.text1);
+        edit_2 = (EditText) findViewById(R.id.text2);
+        edit_1.setOnTouchListener(edithandler);
+        edit_2.setOnTouchListener(edithandler);
+        Button but_plus = (Button)findViewById(R.id.but_plus);
+        Button but_minus = (Button)findViewById(R.id.but_minus);
+        Button but_multiply = (Button)findViewById(R.id.but_multiply);
+        Button but_division = (Button)findViewById(R.id.but_division);
+        TextView result =  (TextView)findViewById(R.id.result);
 
-        for(int i = 0; i < butNums.length; i++){
-            //butNums[i]=(Button)findViewById(ids[i]);
-            butNums[i]=(Button)findViewById(R.id.but0+i);
+        for (int i = 0; i < butNums.length; i++) {
+            butNums[i] = (Button) findViewById(R.id.but0 + i);
             butNums[i].setOnClickListener(butNumHandler);
         }
-
-        for(int i = 0; i < butOps.length; i++){
-            butOps[i] = (Button)findViewById(R.id.but0+i);
-            butOps[i].setOnClickListener(butOpsHandler);
+        for (int i = 0; i < butOps.length; i++) {
+            butOps[i] = (Button) findViewById(R.id.but0 + i);
+            butOps[i].setOnClickListener(butNumHandler);
         }
     }
 
     View.OnClickListener butNumHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Button but = (Button)v;
+            numStr +=but.getText(); // 버튼이 클릭될 때마다 누적
 
+            switch(SelectEdit){
+                case SELECT_EDIT1:
+                    edit_1.setText(numStr);
+                    break;
+                case SELECT_EDIT2:
+                    edit_2.setText(numStr);
+                    break;
+            }
         }
     };
 
     View.OnClickListener butOpsHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            int num1 = Integer.parseInt(edit_1.getText().toString());
+            int num2 = Integer.parseInt(edit_2.getText().toString());
         }
-    }; //익명클래스로 참조값 할당하기 때문에 ; 써줘야함
+    };
+
+    View.OnTouchListener edithandler = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            numStr = "";
+
+            switch(v.getId()){
+                case R.id.text1:
+                    SelectEdit = SELECT_EDIT1;
+                    break;
+                case R.id.text2:
+                    SelectEdit = SELECT_EDIT2;
+                    break;
+            }
+            return true;
+        }
+    };
 }
